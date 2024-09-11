71fed0a0deab93f04c0847a7ff47a03edc144c4f
Argomer-Alcid <mer@cavalry.consulting>
2024-09-12 07:20:05 +0800

    Remove files inside package

--- csgMapProjectBoundary/data/LINE_LEASES_DRILLING_25.js
+++
@@ -1,1 +0,0 @@
-var json_LINE_LEASES_DRILLING_25 = {"type":"FeatureCollection","name":"LINE_LEASES_DRILLING_25","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
